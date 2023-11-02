566d13a2a077b677d1fbb6f1a9bcc2d7414facbc
Diego Romero <dgarrido@ggeedu.mx>
2023-11-02 00:48:50 -0600

    Capitulo context con provider

---
+++ src/context/pokemon/pokemon-provider.tsx
@@ -0,0 +1,40 @@
+import { Slot, component$, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
+import { type PokemonGameState, PokemonGameContext } from './pokemon-game.context';
+import { type PokemonListState, PokemonListContext } from './pokemon-list.context';
+
+export const PokemonProvider = component$(() => {
+    const pokemonGame = useStore<PokemonGameState>({
+		pokemonId: 4,
+		revelar: true,
+		voltear: false
+	});
+
+	//? AQUI estado inicial de ese context
+	const pokemonList = useStore<PokemonListState>({
+		currentPage: 1,
+		isLoading: false,
+		pokemons: []
+	});
+
+    useContextProvider( PokemonGameContext, pokemonGame );
+	useContextProvider( PokemonListContext, pokemonList );
+
+	useVisibleTask$(() => {
+		// TODO: Leer del local storage
+		if( localStorage.getItem('pokemon-game') ) {
+			const { pokemonId = 10, revelar = true, voltear = false } = JSON.parse(localStorage.getItem('pokemon-game')!) as PokemonGameState;
+			pokemonGame.pokemonId = pokemonId;
+			pokemonGame.revelar = revelar;
+			pokemonGame.voltear = voltear;
+		}
+	});
+
+	useVisibleTask$(({ track }) => {
+		/* Cuando se quiere controlr el cambio de más de una variable, es posible colocarlas todas en un array dentro de la funcion track */
+		track(() => [ pokemonGame.revelar, pokemonGame.pokemonId, pokemonGame.voltear ]);
+
+		localStorage.setItem('pokemon-game', JSON.stringify(pokemonGame));
+	});
+
+  return <Slot />
+});
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,36 +1,18 @@
-import { component$, Slot, useContextProvider, useStore, useStyles$ } from "@builder.io/qwik";
-
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import Navbar from "~/components/shared/navbar/navbar";
 
 import styles from "./styles.css?inline";
-import { PokemonGameContext, PokemonListContext } from "~/context";
-import type { PokemonListState, PokemonGameState } from "~/context";
+import { PokemonProvider } from "~/context";
 
 export default component$(() => {
 	useStyles$(styles);
 
-	const pokemonGame = useStore<PokemonGameState>({
-		pokemonId: 4,
-		revelar: true,
-		voltear: false
-	});
-
-	useContextProvider( PokemonGameContext, pokemonGame );
-
-	//? AQUI estado inicial de ese context
-	const pokemonList = useStore<PokemonListState>({
-		currentPage: 1,
-		isLoading: false,
-		pokemons: []
-	});
-
-	useContextProvider( PokemonListContext, pokemonList );
 	return (
-		<>
+		<PokemonProvider>
 			<Navbar />
 			<main class="flex flex-col items-center justify-center">
 				<Slot />
 			</main>
-		</>
+		</PokemonProvider>
 	);
 });
